d9ff5bde0fbb93fbece469095063d29deca5d93f
yvesvanbelle <46778223+yvesvanbelle@users.noreply.github.com>
2019-02-14 11:41:11 +0100

    light yes/no

--- light_sensor.py
+++ light_sensor.py
@@ -1,40 +1,10 @@
-#!/usr/local/bin/python
-
 import RPi.GPIO as GPIO
-import time
 
-__author__ = 'Gus (Adapted from Adafruit)'
-__license__ = "GPL"
-__maintainer__ = "pimylifeup.com"
+PIN_NUMBER = 4
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN_NUMBER, GPIO.IN)
 
-#define the pin that goes to the circuit
-pin_to_circuit = 7
+print(GPIO.input(PIN_NUMBER))
 
-def rc_time (pin_to_circuit):
-    count = 0
-  
-    #Output on the pin for 
-    GPIO.setup(pin_to_circuit, GPIO.OUT)
-    GPIO.output(pin_to_circuit, GPIO.LOW)
-    time.sleep(0.1)
-
-    #Change the pin back to input
-    GPIO.setup(pin_to_circuit, GPIO.IN)
-  
-    #Count until the pin goes high
-    while (GPIO.input(pin_to_circuit) == GPIO.LOW):
-        count += 1
-
-    return count
-
-#Catch when script is interupted, cleanup correctly
-try:
-    # Main loop
-    while True:
-        print rc_time(pin_to_circuit)
-except KeyboardInterrupt:
-    pass
-finally:
-    GPIO.cleanup()
+GPIO.cleanup()
