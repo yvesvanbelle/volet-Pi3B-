1bdb2a0d47bff7d57ffead59652024c2f43b2bdb
yvesvanbelle <46778223+yvesvanbelle@users.noreply.github.com>
2019-03-14 11:11:57 +0100

    add t and h outdoor - cleanup

--- bubendorff.py
+++ bubendorff.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import requests
 import time
 import dht11
 
@@ -21,7 +22,7 @@ def relais(pin):
 	GPIO.cleanup()
 
 
-def temp_humidity(pin):
+def temp_humidity_inside(pin):
 	GPIO.setwarnings(False)
 	GPIO.setmode(GPIO.BCM)
 
@@ -36,14 +37,27 @@ def temp_humidity(pin):
 		time.sleep(1)
 
 
+def temp_humidity_outside():
+    WEATHER_DATA_URL  = 'http://api.openweathermap.org/data/2.5/weather?q=Koekelberg,BE&units=metricAPPID=375e74b400588fcf07d13dbd342093fc'
+
+    weather_data = requests.get(WEATHER_DATA_URL)
+    weather_data = weather_data.json()
+    temp = weather_data['main']['temp']
+    humidity = weather_data['main']['humidity']
+    return temp,humidity
+
 def light(pin):
+	GPIO.setwarnings(False)
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(pin, GPIO.IN)
-	
-	return GPIO.input(pin)
+	light_dark = GPIO.input(pin)
+	GPIO.cleanup()
+	return light_dark
 
 
 if __name__ == '__main__':
-        relais(PINRELAIS)
-        print(light(PINLIGHT))
-        print(temp_humidity(PINTEMP))
+    relais(PINRELAIS)
+    print(light(PINLIGHT))
+    print(temp_humidity_inside(PINTEMP))
+	print(temp_humidity_outside())
+	
